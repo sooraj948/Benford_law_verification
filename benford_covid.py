b8cd600efb108a39dcc540730453f775ff5de11c
sooraj948 <sooraj2.sathish@gmail.com>
2021-03-15 15:08:14 +0530

    made country cmd line arguement

--- benford_covid.py
+++ benford_covid.py
@@ -1,9 +1,11 @@
 import benford as b
 import webscrape_covid_data as ws
+import sys
 
 try:
 
-    country= input("Enter country name : ")
+    # country= input("Enter country name : ")
+    country =  sys.argv[1]
     c=ws.CovidData(country)
 
     l=c.active_case_data()
