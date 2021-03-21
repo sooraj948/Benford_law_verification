5c621de0909a39cd0c691bf1ec8ab713d32e4487
sooraj948 <sooraj2.sathish@gmail.com>
2021-03-21 10:02:25 +0530

    made the choice of graphs as inputs

--- benford_covid.py
+++ benford_covid.py
@@ -8,21 +8,55 @@ try:
     country =  sys.argv[1]
     c=ws.CovidData(country)
 
-    l=c.active_case_data()
+    choice = int(input("Choose one of the following.\n1-Total cases\n2-Daily new cases\n3-Active cases\n4-Deaths\n"))
+
+    if choice==1:
+        l=c.total_case_data()
+        t= b.find_percentage(l)
+        b.plot_bar_line(t[0],t[1],"total case data")
+
+    elif choice==2:
+
+        l=c.daily_new_case_data()
+        t= b.find_percentage(l)
+        b.plot_bar_line(t[0],t[1],"daily new case data")
+
+    elif choice==3:
+
+        l=c.active_case_data()
+        t= b.find_percentage(l)
+        b.plot_bar_line(t[0],t[1],"active case data")
+
+    elif choice==4:
+
+        l=c.deaths_data()
+        t= b.find_percentage(l)
+        b.plot_bar_line(t[0],t[1],"deaths data")
+
+    else:
+        print("pls follow instructions")
+
+
+
+
+
     
-    t= b.find_percentage(l)
     
     # b.plot_line(t[0],t[1])    
 
     # b.plot_bar(t[0],t[1])
 
-    b.plot_bar_line(t[0],t[1],"active case data")
+    
 
 except IndexError:
     print("Pls enter country name also along with the run command. Eg python3 benford_covid.py india")
     
+
 except Exception as e:
     print(e)
+
+except:
+    print("Pls input only the appropriate number")
 
 
 
